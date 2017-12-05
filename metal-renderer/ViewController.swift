6431855efdb642a60ca835397fff9b53cb230905
Marconato Bastien <bastien.marconato@icloud.com>
2017-12-05 17:34:52 +0100

    Initial Commit

---
+++ metal-renderer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  metal-renderer
+//
+//  Created by Bastien Marconato on 05.12.17.
+//  Copyright © 2017 Marconato. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
